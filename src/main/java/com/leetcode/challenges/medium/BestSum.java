2748bfa8227384bed6557bfe87f0cf7ac3c83b60
yourmaingotoguy <khalid.n.mohamed@outlook.com>
2021-08-15 14:31:07 -0400

    Add Best Sum Dynamic Programming problem + Tests

--- src/main/java/com/leetcode/challenges/medium/BestSum.java
+++ src/main/java/com/leetcode/challenges/medium/BestSum.java
@@ -1,22 +1,33 @@
 package com.leetcode.challenges.medium;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 public class BestSum {
-    private static Map<Integer, List<Integer>> memo;
-    private static List<Integer> bestSum;
+    private Map<Integer, List<Integer>> memo;
 
-    public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(1,2,5,25);
-        int targetSum = 100;
-        List<Integer> res = solve(targetSum,numbers);
-        res.stream().forEach(System.out::println);
+    public BestSum(){
+        memo = new HashMap<>();
     }
 
-    public static List<Integer> solve(int targetSum, List<Integer> numbers){
-        if(targetSum == 0) return new ArrayList<Integer>();
+    /**
+     * @param targetSum target sum
+     * @param numbers number array used to reach target sum.
+     * @return an array containing the shortest combination from the numbers array which will add up to targetSum
+     *
+     * depth is at most k
+     * branches at most n times with n being numbers.size()
+     * Without memoization:
+     *   time: O(N^K*K)
+     *   space: O(k^2) because call stack is k and stores k each call.
+     * with memoization
+     *  time: O(N*K^2)
+     *  space: O(k^2)
+     */
+    public List<Integer> solve(int targetSum, List<Integer> numbers){
+        if(memo.containsKey(targetSum)) return memo.get(targetSum);
+        if(targetSum == 0) return new ArrayList<>();
         if(targetSum < 0) return null;
 
         List<Integer> shortestCombination = null;
@@ -24,10 +35,16 @@ public class BestSum {
         for(Integer num : numbers){
             int remainder = targetSum - num;
             List<Integer> remainderCombination = solve(remainder,numbers);
-            if(shortestCombination == null || remainderCombination.size() < shortestCombination.size()){
-                shortestCombination = remainderCombination;
+
+            if(remainderCombination != null) {
+                List<Integer> combination = new ArrayList<>(remainderCombination);
+                combination.add(num);
+                if(shortestCombination == null || combination.size() < shortestCombination.size()){
+                    shortestCombination = combination;
+                }
             }
         }
+        memo.putIfAbsent(targetSum,shortestCombination);
         return shortestCombination;
     }
 }
--- src/test/java/com/leetcode/challenges/medium/BestSumTest.java
+++ src/test/java/com/leetcode/challenges/medium/BestSumTest.java
@@ -1,4 +1,44 @@
 package com.leetcode.challenges.medium;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Timeout;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
+
+@Timeout(value = 20, unit= TimeUnit.MILLISECONDS)
 public class BestSumTest {
+
+    private BestSum bestSum;
+
+    @BeforeEach
+    public void setUp(){
+        bestSum = new BestSum();
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideArgumentsForBestSum")
+    public void bestSumWithHighComplexity(int targetSum, List<Integer> numbers, List<Integer> expectedResult){
+        List<Integer> result = bestSum.solve(targetSum,numbers);
+        Collections.sort(result);  //O(NLogN)
+        assertEquals(expectedResult,result);
+    }
+
+    private static Stream<Arguments> provideArgumentsForBestSum(){
+        return Stream.of(
+            Arguments.of(7,Arrays.asList(5,3,4,7),Arrays.asList(7)),
+            Arguments.of(8,Arrays.asList(2,3,5),Arrays.asList(3,5)),
+            Arguments.of(8,Arrays.asList(1,4,5),Arrays.asList(4,4)),
+            Arguments.of(100,Arrays.asList(1,2,5,25),Arrays.asList(25,25,25,25))
+        );
+    }
 }
+
