4a3ef2ef51553cd621c51c7b442ef557a8970884
yourmaingotoguy <khalid.n.mohamed@outlook.com>
2022-05-02 12:15:00 -0400

    Algorithm to move All instances of target to end of array

--- src/main/java/com/leetcode/challenges/medium/MoveElementToEnd.java
+++ src/main/java/com/leetcode/challenges/medium/MoveElementToEnd.java
@@ -1,2 +1,36 @@
-package com.leetcode.challenges.medium;public class MoveElementToEnd {
+package com.leetcode.challenges.medium;
+import java.util.*;
+
+public class MoveElementToEnd {
+    public static List<Integer> moveElementToEnd(List<Integer> array, int toMove) {
+        int left = 0;
+        int right = array.size()-1;
+
+        while(left < right){
+            if(array.get(right) == toMove){
+                right--;
+            }
+            if(array.get(left) != toMove){
+                left++;
+            }
+            if(array.get(left) == toMove && array.get(right) != toMove){
+                swap(array,left,right);
+                left++;
+                right--;
+            }
+        }
+
+        // Write your code here.
+        return array;
+    }
+
+    public static void swap(List<Integer> array, int left, int right){
+        Integer temp = array.get(left);
+        array.set(left,array.get(right));
+        array.set(right,temp);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(moveElementToEnd(Arrays.asList(2, 4, 2, 5, 6, 2, 2),2).toString());
+    }
 }
